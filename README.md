00ccd2ba50460419b0cc74ee601b98336b61dfb7
Aditya Sista <adityasista@justdials-MacBook-Pro-3.local>
2017-03-25 03:47:18 +0530

    bugfixes: prevent crawler loop

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # webcrawler
-uses graph algorithm (BFS) to crawl and download an entire website
+A very basic web crawler implemented using beautiful soup 4 and native python
+
+extracts urls from any website and using breadth first search traversal, visits each url and in turn extract urls from each webpage and so on...
+with slight modification this can be used to download an entire website.
+
+usage:
+
+```pip install -r requirements.txt```
+
+```python crawler.py http://stanford.edu/```
+
+replace that with any url that you'd like to crawl 
--- crawler.py
+++ crawler.py
@@ -1,25 +1,40 @@
 from bs4 import BeautifulSoup
+from urlparse import urljoin
 import urllib2
 import sys
 
+visited = {}
+
 def extractUrls(url):
-	resp = urllib2.urlopen("http://www.gpsbasecamp.com/national-parks")
+	print url
+	visited[url] = True
+	try:
+		resp = urllib2.urlopen(url)
+	except:
+		return []
 	soup = BeautifulSoup(resp, from_encoding=resp.info().getparam('charset'))
 	urls = []
 
 	for link in soup.find_all('a', href=True):
 		urls.append(link['href'])
+
+	for i in xrange(len(urls)):
+		if urls[i][0:4] != "http":
+			urls[i] = urljoin(url, urls[i])
 	return urls
 
 def crawlBFS(url):
 	queue = [url]
+	
 	while len(queue) > 0:
 		url = queue.pop(0)
+		while url in visited:
+			url = queue.pop(0)
 		newurls = extractUrls(url)
+		
 		for element in newurls:
 			if element not in queue:
-				queue.append(element)
-		print url
+				queue.append(element.strip('/'))
 
 if __name__ == "__main__":
 	crawlBFS(sys.argv[1])
